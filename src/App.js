18520c233c5fd6176b9b2d53c997458003656e85
huzaifa9999 <mhuzaifa.0228@gmail.com>
2022-10-03 01:07:36 +0530

    adding app.jsx

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-
-
+import React,{useEffect} from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
+const akey='a470f8316ec72a42a8ccb407e67697c72e956eca572e1d8b807a3e2338fdd0dc/stage';
 function App() {
+
+  useEffect(() => {
+    alanBtn({
+      key:akey,
+      onCommand:({command,articles}) => {
+        if(command==='newHeadlines'){
+        console.log("yooooo");
+        console.log(articles);
+        }
+      }
+    })
+  },[]);
+
+ 
+  
+ 
+  
   return (
    <>
-
+<h1>pls work hue hue hue</h1>
    </>
   );
 }
