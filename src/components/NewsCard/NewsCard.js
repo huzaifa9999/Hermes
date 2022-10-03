627daea677ecb811d1a7ae7f8291cbd72d6fca70
Harshita <harshitalakhchaura@gmail.com>
2022-10-04 01:18:31 +0530

    News Cards

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -7,19 +7,19 @@ const NewsCard = ({ article: { description, publishedAt, source, title, url, url
     const classes = useStyles();
 
     return (
-        <Card>
-            <CardActionArea>
+        <Card className={classes.card}>
+            <CardActionArea href={url} target='_blank'>
                 <CardMedia className={classes.image} image={urlToImage || 'https://thumbs.dreamstime.com/b/news-newspapers-folded-stacked-word-wooden-block-puzzle-dice-concept-newspaper-media-press-release-42301371.jpg'} />
-                <div>
+                <div className={classes.details}>
                     <Typography variant="body2" color='textSecondary' component="h2">{(new Date(publishedAt)).toDateString()}</Typography>
                     <Typography variant="body2" color='textSecondary' component="h2">{source.name}</Typography>
                 </div>
-                <Typography gutterBottom variant='h5'>{title}</Typography>
-                <CardContent>
+                <Typography className={classes.title} gutterBottom variant='h5'>{title}</Typography>
+                <CardContent >
                     <Typography variant="body2" color='textSecondary' component='p'>{description}</Typography>
                 </CardContent>
             </CardActionArea>
-            <CardActions>
+            <CardActions className={classes.cardActions}>
                 <Button size='small' color='primary'>Learn More</Button>
                 <Typography variant="h5" color='textSecondary'>{i + 1}</Typography>
             </CardActions>
