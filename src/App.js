04b66855f7f7600036e04d8ffe0586de56a4165a
Harshita <harshitalakhchaura@gmail.com>
2022-10-04 18:23:45 +0530

    Cards of front page

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React, { useEffect, useState } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
 import NewsCards from './components/NewsCards/NewsCards';
 import './App.css'
+import useStyles from './style.js';
 
 const akey = 'a470f8316ec72a42a8ccb407e67697c72e956eca572e1d8b807a3e2338fdd0dc/stage';
 function App() {
 
+  const classes = useStyles();
   const [newsArticles, setNewsArticles] = useState([]);
 
   useEffect(() => {
@@ -25,7 +27,9 @@ function App() {
 
   return (
     <>
-      <h1>Alan AI News App</h1>
+      <div className='classes.logoContainer'>
+        <img src='https://voicebot.ai/wp-content/uploads/2019/10/alan.jpg' className={classes.alanLogo}/>
+      </div>
       <NewsCards articles={newsArticles} />
     </>
   );
--- src/components/NewsCards/Styles.js
+++ src/components/NewsCards/Styles.js
@@ -1,9 +1,23 @@
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles({
-    container:{
-        padding:'0 5%',
-        width:'100%',
-        margin:0
-    }
-})
+    card: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: '80%',
+        height: '45vh',
+        padding: '10%',
+        borderRadius: 10,
+        color: 'white',
+        backdropFilter: "blur(30px)",
+        boxShadow: '10px 10px 10px rgba(30,30,30,0.5)',
+    },
+    infoCard: {
+        display: 'flex', flexDirection: 'column', textAlign: 'center',
+    },
+    container: {
+        padding: '0 5%', width: '100%', margin: 0,
+    },
+});
