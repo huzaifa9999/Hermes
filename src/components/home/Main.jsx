a7533100fd89cf4e72525461ecc804c11722e31b
huzaifa9999 <mhuzaifa.0228@gmail.com>
2022-10-14 16:52:23 +0530

    cards added

--- src/components/home/Main.jsx
+++ src/components/home/Main.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect } from "react";
 import Fade from "react-reveal/Fade";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+// import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "./main.css";
-import bgvideo from "../.././assests/b4.mp4"
+import bgvideo from "../.././assests/b4.mp4";
 import Aos from "aos";
 import "aos/dist/aos.css";
-import lo from "../../assests/hermes.svg"
-import logo from '../../assests/hermeslogo.png'
+import left from "../../assests/hermes-left.svg";
+import right from "../../assests/hermes-right.svg";
+import MainCard from "./MainCard";
 
 export default function Main() {
   useEffect(() => {
@@ -14,109 +15,30 @@ export default function Main() {
   }, []);
   return (
     <>
-      <div className="container">
-
-        <video autoPlay loop muted id='video'>
-
-          <source src={bgvideo} type='video/mp4' />
+      <div className="home">
+        <video autoPlay loop muted id="video">
+          <source src={bgvideo} type="video/mp4" />
         </video>
 
-       
         <div className="heading">
-        <div className="img-container">
-              <img
-                src={lo}
-                alt="a"
-                className="image"
-              />
-            </div><h1>
-          {" "}
-          HERMES </h1>
-          
-          <div className="img-container">
-              <img
-                src={lo}
-                alt="a"
-                className="image"
-              />
-            </div>
-          
-          </div>
-        <div className="heading1">
-        
-          <Fade left big cascade>
-
-            <div className="text">
-              <ul class="small-block-grid-2 medium-block-grid-3 flip-cards">
-
-                <li ontouchstart="this.classList.toggle('hover');">
-                  <div class="large button card-front">
-                    <a>Learn More about the App</a>
-                    <i class="fa fa-code card-icon "></i>
-                  </div>
-                  <div class="panel card-back">
-                    <i class="fa fa-code card-icon"></i>
-                    <div class="hub-info">
-                      <a >JKLCode.com</a>
-                      <p>Find coding gists, cheatsheets, plugins, themes, resources, and tutorials.</p>
-                    </div>
-                    <small class="clear">Updated TTH.</small>
-                  </div>
-                </li>
-
-                <li ontouchstart="this.classList.toggle('hover');">
-                  <div class="large button card-front">
-                    <a>Instructions</a>
-                    <i class="fa fa-pencil-square-o card-icon"></i>
-                  </div>
-
-                  <div class="panel card-back">
-                    <i class="fa fa-pencil-square-o card-icon"></i>
-                    <div class="hub-info">
-                      <a>AaronSnowberger.com</a>
-                      <p>Take a look at my graphic design portfolio and contact me for design work.</p>
-                    </div>
-                    <small class="clear">Updated Saturdays.</small>
-                  </div>
-                </li>
-
-                <li ontouchstart="this.classList.toggle('hover');">
-                  <div class="large button card-front">
-                    <a>Get Started</a>
-                    <i class="fa fa-paper-plane-o card-icon"></i>
-                  </div>
-
-                  <div class="panel card-back">
-                    <i class="fa fa-paper-plane-o card-icon"></i>
-                    <div class="hub-info">
-                      <a>Udemy.com</a>
-                      <p>Join classes about Computers, Technology, Coding, Design, and Language Learning.</p>
-                    </div>
-                    <small class="clear">Updated Mondays.</small>
-                  </div>
-                </li>
-              </ul>
-              {/* <span><h2>Welcome to Hermes, a News App as agile and fast as the Greek
-                Messenger of Gods.</h2></span>
-
-              <h3>
-                {" "}
-                With progress in Artificial Intelligence, the ways of reading
-                news is also evolving. So here we present you an innovation to
-                read the news where you only have to speak and hear.
-              </h3>
-              {/* <h3>
-                You can also ask general questions from AI<br></br> For example
-                "How are you?" .
-              </h3> */}
-            </div></Fade>
+          <Fade left cascade>
+            <img src={right} alt="" className="headimage" />
+          </Fade>
+          <h1>
+            <Fade>HERMES</Fade>
+          </h1>
+          <Fade right cascade>
+            <img src={left} alt="" className="headimage" />
+          </Fade>
         </div>
-        {/* <div className="button">
-            <button className="btn">
-              {" "}
-              <Link to={"/news"}>Get Started</Link>
-            </button>
-          </div> */}
+       
+        <div className="heading1">
+       
+       <MainCard />
+        <MainCard />
+        <MainCard />
+
+        </div>
       </div>
     </>
   );
---
+++ src/components/home/MainCard.jsx
@@ -0,0 +1,19 @@
+
+import Fade from "react-reveal/Fade";
+import React from "react";
+import "./maincard.css";
+export default function MainCard() {
+  return (
+    <>  
+    <div class="container">
+
+  <div class="content">
+  <Fade bottom cascade> <h1>Pratham</h1>
+    <h3>Button </h3>
+    </Fade></div>
+<div class="flap"> </div>
+
+</div>  
+    </>
+  );
+}
